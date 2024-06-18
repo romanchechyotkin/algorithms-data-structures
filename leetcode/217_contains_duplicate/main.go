7bfb544fa7f8c04d963b37cace9a1d6fce757a48
romanchechyotkin <romanchechyotkin@gmail.com>
2024-06-18 12:53:07 +0300

    contains duplicates

--- leetcode/217_contains_duplicate/main.go
+++ leetcode/217_contains_duplicate/main.go
@@ -3,22 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(containsDuplicate( []int{1, 2, 2, 4, 5} ))
+	fmt.Println(containsDuplicate([]int{1, 2, 2, 4, 5}))
 	fmt.Println(containsDuplicate([]int{1, 2, 3, 4}))
 	fmt.Println(containsDuplicate([]int{1, 1, 2, 3, 4, 1}))
 }
 
 func containsDuplicate(nums []int) bool {
-    m := make(map[int]int)
+	counter := make(map[int]struct{})
 
-    for _, key := range nums {
-        if m[key] == 1 {
-            return true
-        } else {
-            m[key] = 1                
-        }
-    } 
+	for _, num := range nums {
+		if _, ok := counter[num]; ok {
+			return true
+		} else {
+			counter[num] = struct{}{}
+		}
+	}
 
-    return false
-
+	return false
 }
