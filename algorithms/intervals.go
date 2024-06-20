916ecbe3663a228e8f73e8287587019007ea32a8
romanchechyotkin <romanchechyotkin@gmail.com>
2024-06-20 22:03:37 +0300

    intervals

---
+++ algorithms/intervals.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"sort"
+)
+
+func main() {
+	log.Println(mergeIntervals([][]int{{2, 4}, {1, 5}, {8, 10}, {9, 14}, {15, 19}}))
+	log.Println(mergeIntervals([][]int{{2, 8}, {1, 5}, {8, 10}, {9, 14}, {15, 19}})) 
+	log.Println(mergeIntervals([][]int{{2, 8}, {1, 5}, {8, 10}, {9, 14}, {15, 19}})) 
+	log.Println(mergeIntervals([][]int{{2, 8}, {1, 4}, {4, 5}, {7, 11}, {0, 1}})) 
+}
+
+// [ [1,5], [2,4], [8,10], [9,14], [15, 19] ]
+func mergeIntervals(intervals [][]int) [][]int {
+	var res [][]int
+
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	res = append(res, intervals[0]) // [ [1, 5],  [8, 10]]
+
+	for i := 1; i < len(intervals); i++ {
+		interval := intervals[i] // [2, 4]
+
+		// merge intervals if true 
+		if interval[0] <= res[len(res)-1][1] {
+			if interval[1] >= res[len(res)-1][1] {
+				res[len(res)-1][1] = interval[1]
+			}
+
+			continue
+		}
+
+		// insert new interval
+		res = append(res, interval)
+	}
+
+
+	return res
+}
